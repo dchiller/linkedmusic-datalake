56ced0f925e4ac62c58bf35857d8b6c3749a9159
Dylan Hillerbrand <dhillerbrand@gmail.com>
2023-09-25 13:53:37 -0400

    Add recursive index capability

--- index_data.py
+++ index_data.py
@@ -1,68 +1,66 @@
 import json
+from pyld import jsonld
 import requests
 
-def parse_json_file(json_file):
+_TYPE_MAPPINGS = {
+    'http://www.w3.org/2001/XMLSchema#dateTime': 'dt',
+}
+
+def parse_json_file(json_file: str, expand: bool = False):
     with open(json_file) as f:
-        data = json.load(f)
+        data: list = json.load(f)
+    if expand:
+        data = jsonld.expand(data)
     return data
 
-def post_docs_to_solr(docs):
-    solr_url = 'http://localhost:8983/solr/test_core/update/json/'
-    headers = {'Content-type': 'application/json'}
-    update_json = {"add": docs, "commit": {}}
+def post_docs_to_solr(docs: list):
+    solr_url: str = "http://localhost:8983/solr/test_core/update/json/"
+    headers: dict = {"Content-type": "application/json"}
+    update_json: dict = {"add": docs, "commit": {}}
     r = requests.post(solr_url, json=update_json, headers=headers)
     return r.status_code
 
-def index_docs_cantusdb(file_path):
-    data = parse_json_file(file_path)
-    drop_keys = ["@context"]
-    for doc in data:
-        for key in drop_keys:
-            if key in doc:
-                del doc[key]
-    field_mapper = {
-        "volpiano": "volpiano_t",
-        "@id": "id",
-        "@type": "type",
-        "database": "database_s",
-        "P86": "P86_s", # Composer
-        "P1922": "P1922_s", # Incipit
-        "P136": "P136_s", # Genre
-        "Q731978": "Q731978_s", # Mode
-        "Q4484726": "Q4484726_s", # Final
-        "source": "source_s",
-    }
-    docs = [dict((field_mapper[key], value) for (key, value) in doc.items()) for doc in data]
+def _map_key_types(key: str):
+    key_suffix = _TYPE_MAPPINGS.get(key)
+    if key_suffix:
+        return f"{key}_{key_suffix}"
+    return f"{key}_s"
+
+def _escape_keys(key: str):
+    esc_key = key.replace("_", "9_")
+    esc_key = esc_key.replace("/", "_")
+    return esc_key
+
+def _prep_doc_keys(doc):
+    prepped_doc = {}
+    prepped_doc["@id"] = doc.pop("@id")
+    prepped_doc["@type"] = doc.pop("@type")
+    for key, val in doc.items():
+        key = _escape_keys(key)
+        if len(val) > 1:
+            prepped_rec_docs = [_prep_doc_keys(val_item) for val_item in val]
+            prepped_doc[key] = prepped_rec_docs
+        elif len(val) == 0:
+            continue
+        elif isinstance(val[0], dict):
+            val_dict = val[0]
+            if "@id" in val_dict:
+                prepped_doc[f"{key}_s"] = val_dict["@id"]
+            elif "@value" in val_dict:
+                key_to_index = _map_key_types(key) if "@type" in val_dict else f"{key}_s"
+                val_to_index = val_dict["@value"]
+                prepped_doc[key_to_index] = val_to_index
+    return prepped_doc
+
+def index_docs(jsonld_file_path):
+    data = parse_json_file(jsonld_file_path, expand=True)
+    docs = [_prep_doc_keys(doc) for doc in data]
     post_status = post_docs_to_solr(docs)
     return post_status
 
-def index_docs_simssadb(file_path):
-    data = parse_json_file(file_path)
-    drop_keys = ["@context"]
-    for doc in data:
-        for key in drop_keys:
-            if key in doc:
-                del doc[key]
-    field_mapper = {
-        "@id": "id",
-        "@type": "type",
-        "database": "database_s",
-        #"P86": "P86_ss", # Composer
-        "P136": "P136_s", # Genre
-        "P1476": "P1476_s", # Title
-        "P2701": "P2701_s", # File type,
-        "P214": "P214_i", # VIAF ID
-        "Last_Pitch_Class": "Last_Pitch_Class_s",
-    }
-    docs = []
-    for doc in data:
-        new_doc = {}
-        new_doc["P86_ss"] = [value for value in doc["P86"].values()]
-        new_doc["files"] = [dict((field_mapper[key], value) for (key, value) in file.items()) for file in doc["files"]]
-        for key, value in field_mapper.items():
-            if key in doc:
-                new_doc[value] = doc[key]
-        docs.append(new_doc)
-        print(new_doc)
-    post_status = post_docs_to_solr(docs)
-    return post_status
+def delete_all_docs():
+    solr_url: str = "http://localhost:8983/solr/test_core/update/json/"
+    headers: dict = {"Content-type": "application/json"}
+    update_json: dict = {"delete": {"query": "*:*"}, "commit": {}}
+    r = requests.post(solr_url, json=update_json, headers=headers)
+    return r.status_code
