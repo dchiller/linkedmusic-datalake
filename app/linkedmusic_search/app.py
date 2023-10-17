5b4337196a1b3bc67a17e6cdb0df431970efae7a
Dylan Hillerbrand <dhillerbrand@gmail.com>
2023-10-17 14:06:29 -0400

    Add basic search application

--- app/linkedmusic_search/app.py
+++ app/linkedmusic_search/app.py
@@ -4,42 +4,202 @@ from pyld import jsonld
 
 app = Flask(__name__)
 
+FIELDS_TO_REMOVE = [
+    "@id",
+    "@type",
+    "_version_",
+    "_root_",
+    "_nest_parent_",
+    "text_txt",
+    "wd_label_txt",
+]
 
-def prepare_link(doc):
-    expanded_doc = jsonld.expand(doc)
-    return expanded_doc[0]["@id"]
+
+def get_labels(wd_ids, include_aliases=False):
+    label_qs = "|".join(wd_ids)
+    if include_aliases:
+        wd_api_url = f"https://www.wikidata.org/w/api.php?action=wbgetentities&ids={label_qs}&format=json&props=labels|aliases&languages=en"
+    else:
+        wd_api_url = f"https://www.wikidata.org/w/api.php?action=wbgetentities&ids={label_qs}&format=json&props=labels&languages=en"
+    r = requests.get(wd_api_url).json()
+    r_entities = r["entities"]
+    labels = {}
+    for wd_id, res in r_entities.items():
+        labels[wd_id] = [res["labels"]["en"]["value"]]
+        if include_aliases:
+            aliases = [alias["value"] for alias in res["aliases"]["en"]]
+            labels[wd_id].extend(aliases)
+    return labels
 
 
-def query_solr(search_string, search_field):
-    r = requests.get(
-        f"http://solr:8983/solr/test_core/select?q=*:*&fq={search_field}:{search_string}&rows=30"
-    ).json()
-    result_ids = [prepare_link(query_result) for query_result in r["response"]["docs"]]
-    num_results = r["response"]["numFound"]
-    return result_ids, num_results
+def prepare_results(results):
+    prepped_results = []
+    fields_to_label = set()
+    for res_doc in results:
+        prepped_res = {}
+        prepped_res["link"] = res_doc.pop("@id")
+        prepped_res["type"] = res_doc.pop("@type").split("/")[-1]
+        if prepped_res["type"] != "":
+            fields_to_label.add(prepped_res["type"])
+        prepped_res["int_metadata"] = []
+        prepped_res["metadata"] = []
+        for field in res_doc:
+            if field.startswith("wd") and field.endswith("_t"):
+                wd_id = field.split("_")[1]
+                prepped_res["int_metadata"].append((wd_id, res_doc[field]))
+                fields_to_label.add(wd_id)
+        prepped_results.append(prepped_res)
+    field_label_dict = get_labels(fields_to_label)
+    for res in prepped_results:
+        if res["type"]:
+            res["type"] = field_label_dict[res["type"]][0]
+        for metadata_tup in res["int_metadata"]:
+            res["metadata"].append((field_label_dict[metadata_tup[0]], metadata_tup[1]))
+    return prepped_results
+
+
+def query_solr(search_string, search_fields, search_type):
+    if search_fields:
+        filter_q_search_fields = []
+        q_search_fields = []
+        for search_field in search_fields:
+            search_field = (
+                f"wdt_{search_field}"
+                if search_field.startswith("P")
+                else f"wd_{search_field}"
+            )
+            filter_q_search_fields.append(f"{search_field}_s")
+            q_search_fields.append(f"{search_field}_t")
+    else:
+        filter_q_search_fields = ["text_ss"]
+        q_search_fields = ["text_txt"]
+    rec_query = f"http://solr:8983/solr/test_core/select?q=wd_label_txt:{search_string}&fq=@type:ld_item&wt=json"
+    rec_results = requests.get(rec_query).json()["response"]["docs"]
+    filter_query_add = ""
+    if rec_results:
+        filter_query_add = [
+            f'{filter_q_search_field}:"http://www.wikidata.org/entity/{doc["@id"]}"'
+            for doc in rec_results
+            for filter_q_search_field in filter_q_search_fields
+        ]
+        filter_query_add = " OR ".join(filter_query_add)
+        filter_query_add = " OR " + filter_query_add
+    type_add = ""
+    if search_type:
+        type_add += f'&fq=@type:"http://www.wikidata.org/entity/{search_type}"'
+    q_add = (
+        " OR ".join(
+            [
+                f"&q={q_search_field}:{search_string}"
+                for q_search_field in q_search_fields
+            ]
+        )
+        + filter_query_add
+    )
+    gen_query_str = (
+        f"http://solr:8983/solr/test_core/select?{q_add}{type_add}&wt=json&rows=40"
+    )
+    gen_query_results = requests.get(gen_query_str).json()["response"]
+    num_found = gen_query_results["numFound"]
+    res_docs = gen_query_results["docs"]
+    if num_found > 0:
+        res_docs = prepare_results(res_docs)
+    else:
+        res_docs = []
+    return res_docs, num_found
 
 
 def get_available_fields():
     r = requests.get(
-        f"http://solr:8983/solr/test_core/select?q=*:*&rows=0&facet&wt=csv"
+        f"http://solr:8983/solr/test_core/select?q=*:*&rows=0&facet=true&wt=csv"
     ).text.split(",")
-    r.remove("@id")
-    r.remove("@type")
-    r.remove("@context")
-    r.remove("_version_")
-    r.remove("_root_")
-    r.remove("_nest_parent_")
-    r = [{"field": r_item, "display": " ".join(r_item.split("_")[:-1])} for r_item in r]
-    return r
+    for field in FIELDS_TO_REMOVE:
+        r.remove(field)
+    filtered_fields = set()
+    for field in r:
+        if not field.startswith("wd"):
+            continue
+        filtered_field = field.split("_")[1]
+        filtered_fields.add(filtered_field)
+    filtered_fields = list(filtered_fields)
+    field_labels = get_labels(filtered_fields, include_aliases=True)
+    field_label_pairs = []
+    for field_id, labels in field_labels.items():
+        for label_option in labels:
+            field_label_pairs.append((field_id, label_option))
+    return field_label_pairs
+
+
+def get_available_types():
+    r = requests.get(
+        f"http://solr:8983/solr/test_core/select?q=*:*&rows=0&facet=true&facet.field=@type&wt=json"
+    ).json()
+    avail_types = r["facet_counts"]["facet_fields"]["@type"][0::2]
+    avail_types.remove("ld_item")
+    avail_types.remove("ld_prop")
+    avail_types = [t for t in avail_types if t.startswith("http://www.wikidata.org/")]
+    avail_types = [t.split("/")[-1] for t in avail_types]
+    type_labels = get_labels(avail_types)
+    return type_labels
+
+
+def get_related_search_fields(search_field):
+    search_field_url = (
+        f"http://www.wikidata.org/entity/{search_field}"
+        if search_field.startswith("Q")
+        else f"http://www.wikidata.org/prop/direct/{search_field}"
+    )
+    r = requests.get(
+        f'http://solr:8983/solr/test_core/select?q=*:*&rows=10&fq=@type:ld_similar_terms AND @id:"{search_field_url}"&wt=json'
+    ).json()
+    if r["response"]["numFound"] == 0:
+        return []
+    rel_term_set = set()
+    for rel_term_res in r["response"]["docs"]:
+        if "_nest_parent_" in rel_term_res:
+            rel_term_set.add(rel_term_res["_nest_parent_"].split("/")[-1])
+    rel_term_labels = get_labels(rel_term_set)
+    return rel_term_labels
 
 
 @app.route("/")
 def lmdl_search():
-    fields = get_available_fields()
+    avail_fields = get_available_fields()
+    avail_types = get_available_types()
     if search_string := request.args.get("search-string"):
-        search_field = request.args.get("search-field")
-        results, num_results = query_solr(search_string, search_field)
+        search_field = request.args.getlist("field-value")
+        search_type = request.args.get("search-type")
+        if len(search_field) == 1 and search_field[0] == "":
+            search_field = ""
+        results, num_results = query_solr(search_string, search_field, search_type)
+        if len(search_field) == 1:
+            search_field = search_field[0]
+            related_search_fields = get_related_search_fields(search_field)
+            if related_search_fields:
+                comb_related_search_fields = "&search-value=".join(
+                    [
+                        f"{field_id}"
+                        for field_id, field_label in related_search_fields.items()
+                    ]
+                )
+            else:
+                related_search_fields = ""
+                comb_related_search_fields = ""
+        else:
+            related_search_fields = ""
+            comb_related_search_fields = ""
         return render_template(
-            "search.html", results=results, num_results=num_results, fields=fields
+            "search.html",
+            results=results,
+            num_results=num_results,
+            avail_fields=avail_fields,
+            avail_types=avail_types,
+            related_search_fields=related_search_fields,
+            search_string=search_string,
+            search_field=search_field,
+            search_type=search_type,
+            comb_related_search_fields=comb_related_search_fields,
         )
-    return render_template("search.html", fields=fields)
+    return render_template(
+        "search.html", avail_fields=avail_fields, avail_types=avail_types
+    )
--- index_data.py
+++ index_data.py
@@ -2,93 +2,175 @@ import json
 from pyld import jsonld
 import requests
 
-_TYPE_MAPPINGS = {
-    "http://www.w3.org/2001/XMLSchema#dateTime": "dt",
-}
 
-_AUTHORITY_MAPPINGS = {
-    "http://www.wikidata.org/entity/": "wd",
-    "http://www.wikidata.org/prop/direct/": "wdt",
-    "https://schema.org/": "schema",
-    "https://cantusdatabase.org/chant/": "cdb_chant",
-    "https://cantusdatabase.org/source/": "cdb_source",
-    "https://cantusdatabase.org/about/": "cdb",
-    "https://cantusindex.org/id/": "cid",
-    "https://db.simssa.ca/files/": "simssadb_file",
-    "https://db.simssa.ca/musicalworks/": "simssadb_musicalwork",
-    "https://thesession.org/tunes/": "thesession_tune",
-    "https://thesession.org/tunes/popular/": "thesession_popular",
-}
+class LDIndexer:
+    _TYPE_MAPPINGS = {
+        "http://www.w3.org/2001/XMLSchema#dateTime": "dt",
+    }
 
+    _AUTHORITY_MAPPINGS = {
+        "http://www.wikidata.org/entity/": "wd",
+        "http://www.wikidata.org/prop/direct/": "wdt",
+        "https://schema.org/": "schema",
+        "https://cantusdatabase.org/chant/": "cdb_chant",
+        "https://cantusdatabase.org/source/": "cdb_source",
+        "https://cantusdatabase.org/about/": "cdb",
+        "https://cantusindex.org/id/": "cid",
+        "https://db.simssa.ca/files/": "simssadb_file",
+        "https://db.simssa.ca/musicalworks/": "simssadb_musicalwork",
+        "https://thesession.org/tunes/": "thesession_tune",
+        "https://thesession.org/tunes/popular/": "thesession_popular",
+    }
 
-def parse_json_file(json_file: str) -> list:
-    with open(json_file) as f:
-        data: list = json.load(f)
-    data = jsonld.expand(data)
-    return data
+    _SIMILAR_TERMS_LIST = [
+        [
+            "http://www.wikidata.org/prop/direct/P826",
+            "http://www.wikidata.org/entity/Q731978",
+            "http://www.wikidata.org/entity/Q4484726",
+        ],
+    ]
 
+    def __init__(self):
+        self.items_wdid = set()
+        self.props_wdid = set()
 
-def post_docs_to_solr(docs: list):
-    solr_url: str = "http://localhost:8983/solr/test_core/update/json/"
-    headers: dict = {"Content-type": "application/json"}
-    update_json: dict = {"add": docs, "commit": {}}
-    r = requests.post(solr_url, json=update_json, headers=headers)
-    return r.status_code
+    def parse_json_file(self, json_file: str) -> list[dict]:
+        """
+        Open and expand a JSON-LD file with a remote context.
 
+        Parameters
+        ----------
+        json_file: str
+            Path to JSON-LD file
 
-def _map_key_types(key: str):
-    key_suffix = _TYPE_MAPPINGS.get(key)
-    if key_suffix:
-        return f"{key}_{key_suffix}"
-    return f"{key}_s"
+        Returns
+        -------
+        list
+            List of dictionaries representing JSON-LD data
+        """
+        with open(json_file, encoding="utf-8") as f:
+            data: list = json.load(f)
+        data = jsonld.expand(data)
+        return data
 
+    def post_docs_to_solr(self, docs: list) -> int:
+        """
+        Makes POST request to solr with documents to index.
 
-def _prefix_keys(key: str):
-    rl_prefix = "/".join(key.split("/")[0:-1]) + "/"
-    try:
-        new_key_prefix = _AUTHORITY_MAPPINGS[rl_prefix]
-    except KeyError:
-        print(rl_prefix)
-        print("Ensure that all keys are prefixed with a known authority")
-    new_key_prefix = new_key_prefix.rstrip("_")
-    new_key = f"{new_key_prefix}_{key.split('/')[-1]}"
-    return new_key
+        Parameters
+        ----------
+        docs: list
+            List of dictionaries representing JSON-LD data
 
+        Returns
+        -------
+        int
+            Status code of POST request
+        """
+        solr_url: str = "http://localhost:8983/solr/test_core/update/json/"
+        headers: dict = {"Content-type": "application/json"}
+        update_json: dict = {"add": docs, "commit": {}}
+        r = requests.post(solr_url, json=update_json, headers=headers, timeout=20)
+        return r.status_code
 
-def _prep_doc_keys(doc):
-    prepped_doc = {}
-    prepped_doc["@id"] = doc.pop("@id")
-    prepped_doc["@type"] = doc.pop("@type")[0]
-    for key, val in doc.items():
-        key = _prefix_keys(key)
-        if len(val) > 1:
-            prepped_rec_docs = [_prep_doc_keys(val_item) for val_item in val]
-            prepped_doc[key] = prepped_rec_docs
-        elif len(val) == 0:
-            continue
-        elif isinstance(val[0], dict):
-            val_dict = val[0]
-            if "@id" in val_dict:
-                prepped_doc[f"{key}_s"] = val_dict["@id"]
-            elif "@value" in val_dict:
-                key_to_index = (
-                    _map_key_types(key) if "@type" in val_dict else f"{key}_t"
-                )
-                val_to_index = val_dict["@value"]
-                prepped_doc[key_to_index] = val_to_index
-    return prepped_doc
+    def _map_key_types(self, key: str):
+        key_suffix = self._TYPE_MAPPINGS.get(key)
+        if key_suffix:
+            return f"{key}_{key_suffix}"
+        return f"{key}_s"
 
+    def _prefix_keys(self, key: str):
+        rl_prefix = "/".join(key.split("/")[0:-1]) + "/"
+        try:
+            new_key_prefix = self._AUTHORITY_MAPPINGS[rl_prefix]
+        except KeyError:
+            print(rl_prefix)
+            print("Ensure that all keys are prefixed with a known authority")
+        new_key_prefix = new_key_prefix.rstrip("_")
+        new_key = f"{new_key_prefix}_{key.split('/')[-1]}"
+        return new_key
 
-def index_docs(jsonld_file_path):
-    data = parse_json_file(jsonld_file_path)
-    docs = [_prep_doc_keys(doc) for doc in data]
-    post_status = post_docs_to_solr(docs)
-    return post_status
+    def _prep_doc_keys(self, doc):
+        prepped_doc = {}
+        prepped_doc["@id"] = doc.pop("@id")
+        prepped_doc["@type"] = doc.pop("@type")[0]
+        for key, val in doc.items():
+            key = self._prefix_keys(key)
+            if key.startswith("wd"):
+                self.props_wdid.add(key)
+            if len(val) > 1:
+                prepped_rec_docs = [self._prep_doc_keys(val_item) for val_item in val]
+                prepped_doc[key] = prepped_rec_docs
+            elif len(val) == 0:
+                continue
+            elif isinstance(val[0], dict):
+                val_dict = val[0]
+                if "@id" in val_dict:
+                    prepped_doc[f"{key}_s"] = val_dict["@id"]
+                    if "www.wikidata.org" in val_dict["@id"]:
+                        self.items_wdid.add(val_dict["@id"])
+                if "@value" in val_dict:
+                    key_to_index = (
+                        self._map_key_types(key) if "@type" in val_dict else f"{key}_t"
+                    )
+                    val_to_index = val_dict["@value"]
+                    prepped_doc[key_to_index] = val_to_index
+                if "http://www.wikidata.org/entity/P2561" in val_dict:
+                    prepped_doc[f"{key}_t"] = val_dict[
+                        "http://www.wikidata.org/entity/P2561"
+                    ][0]["@value"]
+        return prepped_doc
 
+    def index_wd_labels(self, entity_type: str):
+        if entity_type == "item":
+            wd_ids = [wd_url.split("/")[-1] for wd_url in self.items_wdid]
+        elif entity_type == "prop":
+            wd_ids = [prefixed_key.split("_")[-1] for prefixed_key in self.props_wdid]
+        idx_as_type = f"ld_{entity_type}"
+        docs = []
+        for i in range(0, len(wd_ids), 50):
+            wd_ids_slice = wd_ids[i : i + 50]
+            wd_ids_str = "|".join(wd_ids_slice)
+            wd_api_url = f"https://www.wikidata.org/w/api.php?action=wbgetentities&ids={wd_ids_str}&format=json&props=labels|aliases"
+            r = requests.get(wd_api_url).json()
+            r_entities = r["entities"]
+            for wd_id, res in r_entities.items():
+                labels = [label["value"] for key, label in res["labels"].items()]
+                alias_list = []
+                for lang, lang_aliases in res["aliases"].items():
+                    alias_list.extend([alias["value"] for alias in lang_aliases])
+                labels.extend(alias_list)
+                doc = {"@id": wd_id, "@type": idx_as_type, "wd_label_txt": labels}
+                docs.append(doc)
+        post_status = self.post_docs_to_solr(docs)
+        return post_status
 
-def delete_all_docs():
-    solr_url: str = "http://localhost:8983/solr/test_core/update/json/"
-    headers: dict = {"Content-type": "application/json"}
-    update_json: dict = {"delete": {"query": "*:*"}, "commit": {}}
-    r = requests.post(solr_url, json=update_json, headers=headers)
-    return r.status_code
+    def index_docs(self, jsonld_file_path):
+        data = self.parse_json_file(jsonld_file_path)
+        docs = [self._prep_doc_keys(doc) for doc in data]
+        post_status = self.post_docs_to_solr(docs)
+        return post_status
+
+    def delete_all_docs(self):
+        solr_url: str = "http://localhost:8983/solr/test_core/update/json/"
+        headers: dict = {"Content-type": "application/json"}
+        update_json: dict = {"delete": {"query": "*:*"}, "commit": {}}
+        r = requests.post(solr_url, json=update_json, headers=headers, timeout=20)
+        return r.status_code
+
+    def index_similar_terms(self):
+        similar_terms_objs = []
+        for term_group in self._SIMILAR_TERMS_LIST:
+            for term in term_group:
+                term_dict = {
+                    "@id": term,
+                    "@type": "ld_similar_terms",
+                    "wdt_P460": [
+                        {"@id": other_term, "@type": "ld_similar_terms"}
+                        for other_term in term_group
+                        if other_term != term
+                    ],
+                }
+                similar_terms_objs.append(term_dict)
+        post_status = self.post_docs_to_solr(similar_terms_objs)
+        return post_status
