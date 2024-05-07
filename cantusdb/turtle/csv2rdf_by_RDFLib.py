c49469c165ec302887a122e9dce32ed7e103992a
candlecao <529360500@qq.com>
2024-05-07 12:08:48 -0400

    Update csv2rdf_by_RDFLib.py
    Acording to the tips from dchiller, re-upload the python file.
    
    Co-Authored-By: Yueqiao Zhang <113382721+Yueqiao12Zhang@users.noreply.github.com>

--- cantusdb/turtle/csv2rdf_by_RDFLib.py
+++ cantusdb/turtle/csv2rdf_by_RDFLib.py
@@ -10,17 +10,15 @@ prefix_uri = {
     "cantusDB": "https://cantusdatabase.org/",
 }
 
+P_INCIPIT = URIRef(f"{prefix_uri['wdt']}P1922")
+P_GENRE = URIRef(f"{prefix_uri['wdt']}P136")
+P_SRC = URIRef(f"{prefix_uri['cantusDB']}sources")
+CHANT_TYPE = URIRef(f"{prefix_uri['wdt']}chant")
+
 def convert_csv_to_turtle(csv_filename):
     with open(csv_filename, 'r', encoding='utf-8') as csv_file:
         g = Graph()
         csv_reader = csv.DictReader(csv_file)
-        
-        chant_type = URIRef(prefix_uri["cantusDB"] + "chant")
-        
-        # Add predicate URIs
-        p_incipit = URIRef(prefix_uri["wdt"] + "P1922")
-        p_genre = URIRef(prefix_uri["wdt"] + "P136")
-        p_src = URIRef(prefix_uri["cantusDB"] + "sources")
 
         # Convert each row to Turtle format and add it to the output
         for row in csv_reader:
@@ -36,15 +34,15 @@ def convert_csv_to_turtle(csv_filename):
             src_link = URIRef(row["src_link"])
             
             # Add triples
-            g.add((chant_uri, RDF.type, chant_type))
-            g.add((chant_uri, p_incipit, incipit))
-            g.add((chant_uri, p_genre, genre))
-            g.add((chant_uri, p_src, src_link))
+            g.add((chant_uri, RDF.type, CHANT_TYPE))
+            g.add((chant_uri, P_INCIPIT, incipit))
+            g.add((chant_uri, P_GENRE, genre))
+            g.add((chant_uri, P_SRC, src_link))
             
         return g
 
 
-csv_filename = 'reconciled_cantus_01102024_updated_simplified.csv'
+csv_filename = 'reconciled_cantus_01102024_updated_simplified_samples.csv'
 
 # Convert the CSV data to Turtle format and print the result
 turtle_data = convert_csv_to_turtle(csv_filename)
